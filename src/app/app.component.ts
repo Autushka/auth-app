50790ec44e855beb6f93c637628eedfaedd587ef
aleh.autushka@gmail.com <aautushk@teksystems.com>
2016-10-08 18:26:13 -0400

    integration with firebase added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import {Component} from '@angular/core';
 import {Auth} from './auth.service';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
-  providers: [ Auth ],
+  providers: [Auth],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'guest book';
+  guestMessages: FirebaseListObservable<any>;
 
-  constructor(private auth: Auth) {
+  constructor(private auth: Auth, af: AngularFire) {
+    this.guestMessages = af.database.list('/guestMessages');
+  }
+
+  addMessage(newMessage: string) {
+    this.guestMessages.push({content: newMessage})
+  }
+
+  onMessageInputKeyUp(keyCode: number, message: string) {
+    if (keyCode === 13 && message !== '') {
+      this.addMessage(message);
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AUTH_PROVIDERS }      from 'angular2-jwt';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyCuC97XN2ccaTsCLsxqYMDoZqFQ-RIGWO8",
+  authDomain: "authapp-400c9.firebaseapp.com",
+  databaseURL: "https://authapp-400c9.firebaseio.com",
+  storageBucket: "authapp-400c9.appspot.com",
+  messagingSenderId: "161010707572"
+};
 
 @NgModule({
   declarations: [
@@ -13,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [AUTH_PROVIDERS],
   bootstrap: [AppComponent]
