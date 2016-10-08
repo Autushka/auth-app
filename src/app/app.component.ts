fc5022dc3143a38eac5e17da5d904bb41df3bb1c
aleh.autushka@gmail.com <aautushk@teksystems.com>
2016-10-08 09:33:22 -0400

    integration with auth0 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Auth} from './auth.service';
 
 @Component({
   selector: 'app-root',
+  providers: [ Auth ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private auth: Auth) {
+  }
 }
